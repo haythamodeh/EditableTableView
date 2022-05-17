f92f845d2158968f37ff97590ea0b9d6e43e7d31
Haytham <haythamlion@outlook.com>
2022-05-17 16:14:05 -0700

    adds countries model and data storage, adds edit toggle, adds new country randomly when clicking add new country button

---
+++ EditableTableView/Country.swift
@@ -0,0 +1,17 @@
+//
+//  Country.swift
+//  EditableTableView
+//
+//  Created by Student Account on 5/17/22.
+//
+
+import Foundation
+
+
+class Country {
+    var name: String!
+    
+    init(name: String) {
+        self.name = name
+    }
+}
--- EditableTableView/ViewController.swift
+++ EditableTableView/ViewController.swift
@@ -7,11 +7,53 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
+    var data : CountriesDB!
+    var countries = ["Canada", "Portugal", "Chile", "China", "Russia", "Australia"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func addCountry() {
+        if (countries.count > 0) {
+            // add random country
+            let randomIndex = Int(arc4random_uniform(UInt32(countries.count)))
+            data.addCountry(name: countries[randomIndex])
+            //update tableview row
+            let indexPath = IndexPath(row: data.countries.count-1, section: 0)
+            tableView.insertRows(at:[indexPath], with: .automatic)
+
+            //remove random country from country array
+            countries.remove(at: randomIndex)
+            
+            
+        }
+    }
+    @IBAction func EditToggle(_ sender: UIButton) {
+        if isEditing {
+            setEditing(false, animated: true)
+            sender.setTitle("Edit", for: .normal)
+        }
+        else {
+            setEditing(true, animated: true)
+            sender.setTitle("Done", for: .normal)
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.countries.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        cell.textLabel?.text = data.countries[indexPath.row].name
+        cell.detailTextLabel?.text = ""
+        
+        return cell
     }
 
 
